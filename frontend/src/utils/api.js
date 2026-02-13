e577835887bea9ff3337906d109841ec5393b9bf
BETÜL ALBAYRAK <52135580+BetulAlbayrak1999@users.noreply.github.com>
2026-02-13 23:11:54 +0300

    Change API base URL to use environment variable

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+constconst api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
   headers: { 'Content-Type': 'application/json' },
 });
 
